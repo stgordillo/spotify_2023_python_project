5cb2430443618b2f000a45433f99e2cc72f7d5ea
Tommy Gordillo <138537215+stgordillo@users.noreply.github.com>
2024-09-10 16:13:38 -0700

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -11,11 +11,10 @@ df.head()
 df.info()
 df.describe()
 
-# Removing columns for new dataframe
+# Removing columns and exploring for new dataframe
 da = df.loc[:, ["track_name","artist(s)_name","artist_count","released_year","in_spotify_playlists", "in_spotify_charts", "streams", "danceability_%"]]
 da.head()
 
-# Exploring new dataframe
 da.info()
 da.describe()
 
@@ -23,10 +22,9 @@ da.describe()
 da[da.duplicated()]
 da.isnull().sum()
 
-# Creating dataset for top 10 artists
+# Creating viz for top 10 artists
 top_ten = da["artist(s)_name"].value_counts().head(10)
 
-# Creating the dataset for Top 10
 plt.figure(figsize=(12, 6))
 plt.barh(top_ten.index[::-1], top_ten.values[::-1], color="purple")
 plt.xlabel("Amount of Songs")
@@ -36,17 +34,16 @@ plt.show()
 
 print("Here is the list of the Top 10 Artists and how many songs they had in 2023:\n", top_ten)
 
-# Checking the data type for streams to see why I can't plot the data.
+# Troubleshooting data type to find why "streams" cannot be sorted by it's values 
 da["streams"].dtype
 
-# Changing the data types in streams into useable numbers
+# Changing the data types in "streams" from objects into useable numbers
 da['streams'] = pd.to_numeric(da['streams'], errors="coerce")
 da["streams"].dtype
 
-# Creating dataset for top 10 songs
+# Creating viz for 10 most streamed songs
 most_streamed = da[["track_name", "artist(s)_name", "streams"]].sort_values(by="streams", ascending=False).head(10)
 
-# Creating dataset for 10 most streamed songs
 plt.figure(figsize=(12, 6))
 plt.bar(most_streamed["track_name"], most_streamed["streams"], color="orange")
 plt.xlabel("Number of Streams (In Billions)")
@@ -58,9 +55,9 @@ plt.show()
 print("Here is the list of the Top 10 most streamed songs:\n")
 most_streamed
 
+# Creating pie chart viz
 most_streamed = da[['track_name', 'streams']].sort_values(by='streams', ascending=False).head(10)
 
-# Creating pie chart
 plt.figure(figsize=(10, 8))
 plt.pie(most_streamed['streams'], labels=most_streamed['track_name'], autopct='%1.1f%%', colors=plt.cm.tab10.colors)
 plt.title('Most Streamed Songs on Spotify in 2023')
@@ -72,7 +69,7 @@ most_streamed
 # Averaging danceability
 avg_dance = da.groupby("released_year")["danceability_%"].mean()
 
-# Creating line chart
+# Creating line chart viz
 plt.figure(figsize=(10, 6))
 sns.lineplot(x=avg_dance.index, y=avg_dance.values, color="purple")
 plt.xlabel("Year")
